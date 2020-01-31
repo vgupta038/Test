cbaac348d82c4634eb624e86fad45923e6615b0c
vgupta038 <vgupta038@gmail.com>
2020-01-31 20:33:50 +0530

    change1 in master branch

--- src/git_test/GIT_TEST.java
+++ src/git_test/GIT_TEST.java
@@ -16,6 +16,7 @@ public class GIT_TEST {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("change1 in master branch");
     }
     
 }
